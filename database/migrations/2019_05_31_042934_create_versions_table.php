830f31b356bafd8444a6fbc1264b2667ba520c01
Martin Christiansen <code@juul.xyz>
2025-01-01 00:00:00 +0100

    Add versioning and Passport support
    Implemented versioning for the Album model using the Laravel Versionable package, including respective configurations and migrations. Added Laravel Passport for OAuth2 authentication support, updated dependencies, and included necessary migrations and configuration files.

---
+++ database/migrations/2019_05_31_042934_create_versions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('versions', function (Blueprint $table) {
+            $uuid = config('versionable.uuid');
+
+            $uuid ? $table->uuid('id')->primary() : $table->bigIncrements('id');
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
+            $uuid ? $table->uuidMorphs('versionable') : $table->morphs('versionable');
+
+            $table->jsonb('contents')->nullable();
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('versions');
+    }
+};
